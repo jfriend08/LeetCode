a339447c7f45b8f0a9504156f5dca9e744fe771d
jfriend08 <ys486@cornell.edu>
2015-01-10 15:46:32 -0500

    BinaryTreeInorderTraversal(recurrent)_4ms

---
+++ BinaryTreeInorderTraversal.cpp
@@ -0,0 +1,78 @@
+/*
+Compare two version numbers version1 and version1.
+If version1 > version2 return 1, if version1 < version2 return -1, otherwise return 0.
+
+You may assume that the version strings are non-empty and contain only digits and the . character.
+The . character does not represent a decimal point and is used to separate number sequences.
+For instance, 2.5 is not "two and a half" or "half way to version three", it is the fifth second-level revision of the second first-level revision.
+
+Here is an example of version numbers ordering:
+0.1 < 1.1 < 1.2 < 13.37
+
+*/
+
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <vector> 
+#include <list>
+#include <sstream>
+#include <iterator>
+#include <map> 
+#include <iomanip>
+#include <utility> 
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <ctype.h>
+#include <limits>
+#include <queue>
+#include <cstdlib>
+
+using namespace std;
+
+/**
+ * Definition for binary tree
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode *root) {
+    	vector <int> inordervector;
+    	if (root){
+    		if (root->left){
+    			inordervector= inorderTraversal(root->left);
+    		}
+    		
+    		inordervector.push_back(root->val);
+
+    		if (root->right){
+    			vector<int> tmp=inorderTraversal(root->right);	
+    			inordervector.insert(inordervector.end(),tmp.begin(),tmp.end());
+    		}
+    		return inordervector;
+    	}
+    }
+};
+
+
+int main(){
+  string 
+  Solution sol;    
+
+}
+
+
+
+
+
+
+
+
+
