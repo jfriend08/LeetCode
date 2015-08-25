24fee89e6c26781d1fdb1a16cb43ea7ccdf6aae4
jfriend08 <ys486@cornell.edu>
2015-08-24 20:50:15 -0400

    ContainsDuplicate

---
+++ ContainsDuplicate.py
@@ -0,0 +1,15 @@
+'''
+Given an array of integers, find if the array contains any duplicates.
+Your function should return true if any value appears at least twice in the array,
+and it should return false if every element is distinct.
+'''
+
+class Solution(object):
+    def containsDuplicate(self, nums):
+        dupDict = {}
+        for idx in range(len(nums)):
+          try:
+            dupDict[nums[idx]] += 1
+            return True
+          except:
+            dupDict[nums[idx]] = 1
