1eeba2573edcd48e283adc62f30050bafd2161a2
jfriend08 <ys486@cornell.edu>
2015-09-13 15:52:38 -0400

    # Definition for singly-linked list.

---
+++ LinkedListCycle.py
@@ -0,0 +1,20 @@
+'''
+Given a linked list, determine if it has a cycle in it.
+
+Follow up:
+Can you solve it without using extra space?Linked List Cycle
+'''
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def hasCycle(self, head):
+        """
+        :type head: ListNode
+        :rtype: bool
+        """
+        
